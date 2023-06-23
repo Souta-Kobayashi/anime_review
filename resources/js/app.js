5cc3d6f8eb37167d4f3df03af1d94ffbb52e06a0
Souta Kobayashi <soutak.work@gmail.com>
2023-06-23 20:56:21 +0900

    font awesomeパッケージ導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,13 @@ import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
 
+/* fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+library.add([faGithub, faTwitter]);
+
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 app.mount("#app");
