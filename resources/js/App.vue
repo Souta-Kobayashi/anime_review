97804d5efa1e113c696f00a6296ba1f7dee09f40
Souta Kobayashi <soutak.work@gmail.com>
2023-06-23 22:13:44 +0900

    footer追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import BaseHeader from "./components/BaseHeader.vue";
+import BaseFooter from "./components/BaseFooter.vue";
 import { RouterLink, RouterView, useRouter, useRoute } from "vue-router";
 </script>
 
 <template>
   <base-header />
   <router-view></router-view>
+  <base-footer />
 </template>
--- resources/js/components/BaseFooter.vue
+++ resources/js/components/BaseFooter.vue
@@ -1,9 +1,29 @@
-<script setup>
-</script>
-
 <template>
-  <footer>
-    <p>Footer</p>
-    <!-- フッターのコンテンツ -->
+  <footer class="text-center text-white">
+    <!-- SNS Icons -->
+    <div class="container pt-4">
+      <section class="mb-4">
+        <!-- Twitter -->
+        <a
+          class="btn btn-link btn-lg text-dark m-1"
+          href="https://twitter.com/soutaAppservice"
+          target="_blank" rel="noopener noreferrer"
+          role="button"
+          ><font-awesome-icon :icon="['fab', 'twitter']" />
+        </a>
+        <!-- Github -->
+        <a
+          class="btn btn-link btn-lg text-dark m-1"
+          href="https://github.com/Souta-Kobayashi"
+          target="_blank" rel="noopener noreferrer"
+          role="button"
+          ><font-awesome-icon :icon="['fab', 'github']" />
+        </a>
+      </section>
+    </div>
+    <!-- Copyright -->
+    <div class="text-center text-dark p-3 copy-right">
+      © 2023 Souta Kobayashi
+    </div>
   </footer>
 </template>
