87961e10b205a1ec5a3d7e84a906674a944a222d
Souta Kobayashi <soutak.work@gmail.com>
2023-11-26 23:15:05 +0900

    categoryモデル追加、モデル間リレーション追加

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,25 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Category;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Video extends Model
+class Category extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
     protected $fillable = [
         'name',
-        'broadcast_date',
-        'genre',
-        'synopsis',
-        'key_visual_url',
-        'key_visual_reference',
     ];
 
-    public function categories(): BelongsToMany
+    public function videos(): BelongsToMany
     {
-        return $this->BelongsToMany(Category::class, 'videos_categories');
+        return $this->belongsToMany(Video::class, 'videos_categories');
     }
 }
