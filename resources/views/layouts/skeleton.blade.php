eb2c45f359b8ff6a70d7a58e5a287b1dc17951a3
Souta Kobayashi <soutak.work@gmail.com>
2023-12-17 11:26:02 +0900

    headタグの修正

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -4,44 +4,23 @@
     <base href="{{ url('/') }}/" />
 
     <meta charset="utf-8">
-    <meta name="apple-mobile-web-app-title" content="AnimeReview">
+    <meta name="apple-mobile-web-app-title" content="Anime Review">
     <meta name="application-name" content="AnimeReview">
-    <meta name="theme-color" content="#0b0c2a">
+    <meta name="theme-color" content="#343a49">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex">
 
     <title>@yield('title', 'AnimeReview | オススメのアニメを教えてください')</title>
     @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
-    {{-- <script src="{{ asset('js/app.js') }}" defer="defer"></script> --}}
 
-    {{-- <link rel="stylesheet" href="{{ asset(mix('css/app-'.htmldir().'.css')) }}"> --}}
-    <link rel="apple-touch-icon" sizes="152x152" href="img/icons/touch-icon-ipad.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/icons/touch-icon-iphone-retina.png">
-    <link rel="apple-touch-icon" sizes="167x167" href="img/icons/touch-icon-ipad-retina.png">
-
-    <link rel="shortcut icon" sizes="196x196" href="img/icons/favicon-196.png">
+    <link rel="shortcut icon" sizes="50x50" href="image/favicon.ico">
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    
-    {{-- 不要なら削除する --}}
-    {{-- <script>
-      window.Laravel = {!! \Safe\json_encode([
-          'locale' => \App::getLocale(),
-          'htmldir' => htmldir(),
-          'profileDefaultView' => auth()->user()->profile_active_tab,
-          'timezone' => auth()->user()->timezone,
-          'env' => \App::environment(),
-      ]); !!}
-    </script> --}}
   </head>
   <body>
-    <div id="app" class="flex-grow-1">
+    <div id="app">
     </div>
-
-    {{-- <script src="{{ asset(mix('js/manifest.js')) }}"></script>
-    <script src="{{ asset(mix('js/vendor.js')) }}"></script>
-    <script src="{{ asset(mix('js/app.js')) }}"></script> --}}
-
   </body>
 </html>
