04e482484c9c43426e186c515b58ddec1e9c07ab
Souta Kobayashi <soutak.work@gmail.com>
2024-01-08 01:04:52 +0900

    タスクスケジュールを強制実行するように変更

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('migrate:refresh')->weekly();
-        $schedule->command('db:seed')->weekly();
+        $schedule->command('migrate:refresh --force')->everyMinute();
+        $schedule->command('db:seed --force')->everyMinute();
     }
 
     /**
