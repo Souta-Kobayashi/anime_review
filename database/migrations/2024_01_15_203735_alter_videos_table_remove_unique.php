59a07fd1d932a8332ae39079079de8a9a63b1374
Souta Kobayashi <soutak.work@gmail.com>
2024-01-22 22:41:35 +0900

    videosのmigrationのrollback時の処理変更

--- database/migrations/2024_01_15_203735_alter_videos_table_remove_unique.php
+++ database/migrations/2024_01_15_203735_alter_videos_table_remove_unique.php
@@ -21,8 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('videos', function (Blueprint $table) {
-            $table->unique('name');
-        });
+        // Schema::dropIfExists('videos');
     }
 };
