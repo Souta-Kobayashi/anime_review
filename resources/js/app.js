d3e5a7b3cd63bd0fbb7b371b589fbf5126d0ffb9
Souta Kobayashi <soutak.work@gmail.com>
2024-02-12 21:34:02 +0900

    vuetifyバージョンアップデート、sass loader追加、アニメカードにskeleton loader追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,6 @@ import App from './App.vue';
 // Vuetify
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
-// import * as components from 'vuetify/components';
-// import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 
