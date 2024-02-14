1f0b4716c008ab93a8df8ed891817c026273824d
Souta Kobayashi <soutak.work@gmail.com>
2024-02-14 22:08:09 +0900

    登録画面の規約チェックボックス削除、カテゴリ一覧にスケルトンローダー追加

---
+++ resources/js/organisms/CategoriesTableSkeltonLoader.vue
@@ -0,0 +1,32 @@
+<template>
+  <template v-if="isFetching">
+    <tr>
+      <td>
+        <v-skeleton-loader
+          type="table-heading"
+        ></v-skeleton-loader>
+      </td>
+    </tr>
+
+    <tr v-for="index in categoryTableRowCount" :key="index">
+      <td>
+        <v-skeleton-loader
+          type="text,divider"
+        ></v-skeleton-loader>
+      </td>
+    </tr>
+  </template>
+</template>
+
+<script setup>
+const props = defineProps({
+  isFetching: {
+    type: Boolean,
+    default: true,
+  },
+  categoryTableRowCount: {
+    type: Number,
+    default: 20,
+  },
+});
+</script>
