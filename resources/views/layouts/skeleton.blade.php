9901332319a78014da8ab314033646f2ab491d50
Souta Kobayashi <soutak.work@gmail.com>
2024-02-17 18:24:26 +0900

    web fontsを修正 サブセット、セルフホスティング化

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -14,10 +14,6 @@
     @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
 
     <link rel="shortcut icon" sizes="50x50" href="image/favicon.ico">
-
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   </head>
   <body>
     <div id="app">
